ec966270ed60e4980e748300bded004303cc6a5a
unknown <kammar.cloudelitedevtest@gmail.com>
2022-04-07 02:43:35 -0400

    updates

--- Main.tf
+++ Main.tf
@@ -15,10 +15,10 @@ locals {
     component_name          = "IAC-TERRAFORM-REPO"
 
   }
-  vpc_id = aws_vpc.D-Ammar.id
+  vpc_id = aws_vpc.D-Ammar-Inc.id
 }
 
-resource "aws_vpc" D-Ammar {
+resource "aws_vpc" D-Ammar-Inc {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = false
   enable_dns_hostnames = false
