dbbf9d37024866f8bf49a796dfc7f313c1065675
unknown <kammar.cloudelitedevtest@gmail.com>
2022-04-07 00:46:53 -0400

    Assignments

--- Main.tf
+++ Main.tf
@@ -1,4 +1,23 @@
 
+locals {
+  mandatory_tag = {
+    line_of_business        = "hospital"
+    ado                     = "max"
+    tier                    = "WEB"
+    operational_environment = upper(terraform.workspace)
+    tech_poc_primary        = "kingsleyammar660@gmail.com"
+    tech_poc_secondary      = "kingsleyammar660@gmail.com"
+    application             = "http"
+    builder                 = "kingsleyammar660@gmail.com"
+    application_owner       = "kojitechs.com"
+    vpc                     = "WEB"
+    cell_name               = "WEB"
+    component_name          = "IAC-TERRAFORM-REPO"
+
+  }
+  vpc_id = aws_vpc.D-Ammar.id
+}
+
 resource "aws_vpc" D-Ammar {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = false
@@ -42,3 +61,13 @@ resource "aws_subnet" database_subnet2 {
   cidr_block = var.db_subnet_cidr[1]
   availability_zone = var.db_az_cidr[1]
 }
+
+
+
+
+variable "subnet_id" {}
+
+data "aws_route_table" "selected" {
+  subnet_id = var.subnet_id
+}
+
